Forces are now reasonable but this is because there is no buffer for cut-off from central atom! Also, TS forces are wrong in MBD part. Should be derivative of total TS energy

runs/c60_dimers.py
@@ -3,7 +3,7 @@ from ase.io import read, write
 
 d_arr = np.linspace(8,15,36)
 
-atoms1 = read("c60.xyz")
+atoms1 = read("dimer.xyz")
 
 for d in d_arr:
     atoms2 = read("c60.xyz")