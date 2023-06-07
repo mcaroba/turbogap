cebad6c6b4d5b261d1ae5fe62f2f8b92fdaf242a
TiganyZ <tiganyzarrouk@gmail.com>
2023-06-07 13:50:20 +0300

    Forgot to add local_properties.f90 to commit!

---
+++ src/local_properties.f90
@@ -0,0 +1,145 @@
+! HND XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+! HND X
+! HND X   TurboGAP
+! HND X
+! HND X   TurboGAP is copyright (c) 2019-2023, Miguel A. Caro and others
+! HND X
+! HND X   TurboGAP is published and distributed under the
+! HND X      Academic Software License v1.0 (ASL)
+! HND X
+! HND X   This file, local_properties.f90, is copyright (c) 2019-2023, Miguel A.
+! HND X   Caro, Heikki Muhli and Tigany Zarrouk
+! HND X
+! HND X   TurboGAP is distributed in the hope that it will be useful for non-commercial
+! HND X   academic research, but WITHOUT ANY WARRANTY; without even the implied
+! HND X   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+! HND X   ASL for more details.
+! HND X
+! HND X   You should have received a copy of the ASL along with this program
+! HND X   (e.g. in a LICENSE.md file); if not, you can write to the original
+! HND X   licensor, Miguel Caro (mcaroba@gmail.com). The ASL is also published at
+! HND X   http://github.com/gabor1/ASL
+! HND X
+! HND X   When using this software, please cite the following reference:
+! HND X
+! HND X   Miguel A. Caro. Phys. Rev. B 100, 024112 (2019)
+! HND X
+! HND XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+module local_prop
+
+
+  contains
+
+
+    !**************************************************************************
+
+  subroutine local_property_predict( soap, Qs, alphas, V0, delta, zeta, V, &
+                                do_derivatives, soap_cart_der, n_neigh, V_der )
+
+    implicit none
+
+!   Input variables
+    real*8, intent(in) :: soap(:,:), Qs(:,:), alphas(:), V0, delta, zeta, soap_cart_der(:,:,:)
+    integer, intent(in) :: n_neigh(:)
+    logical, intent(in) :: do_derivatives
+!   Output variables
+    real*8, intent(out) :: V(:), V_der(:,:)
+!   Internal variables
+    real*8, allocatable :: K(:,:), K_der(:,:), Qss(:,:), Qs_copy(:,:)
+    integer :: n_sites, n_soap, n_sparse, zeta_int, n_pairs
+    integer :: i, j, i2, cart
+    logical :: is_zeta_int = .false.
+
+    n_sparse = size(alphas)
+    n_soap = size(soap, 1)
+    n_sites = size(soap, 2)
+
+    allocate( K(1:n_sites, 1:n_sparse) )
+    if( do_derivatives )then
+      n_pairs = size(soap_cart_der, 3)
+      allocate( K_der(1:n_sites, 1:n_sparse) )
+      allocate( Qss(1:n_sites, 1:n_soap) )
+      allocate( Qs_copy(1:n_soap, 1:n_sparse) )
+    end if
+
+
+    if( n_sites > 0 )then
+      call dgemm( "t", "n", n_sites, n_sparse, n_soap, 1.d0, soap, n_soap, Qs, n_soap, 0.d0, &
+                K, n_sites)
+    end if
+
+    zeta_int = nint(zeta)
+    if( dabs( dfloat(zeta_int) - zeta ) < 1.d-10 )then
+      if( do_derivatives )then
+        K_der = zeta_int*K**(zeta_int-1)
+        if( n_sites < n_soap)then
+          do i = 1, n_sites
+            K_der(i,:) = K_der(i,:) * alphas(:)
+            Qs_copy = Qs
+          end do
+        else
+          do i = 1, n_soap
+            Qs_copy(i,:) = Qs(i,:) * alphas(:)
+          end do
+        end if
+      end if
+      K = K**zeta_int
+    else
+      if( do_derivatives )then
+        K_der = zeta*K**(zeta-1.d0)
+        if( n_sites < n_soap)then
+          do i = 1, n_sites
+            K_der(i,:) = K_der(i,:) * alphas(:)
+            Qs_copy = Qs
+          end do
+        else
+          do i = 1, n_soap
+            Qs_copy(i,:) = Qs(i,:) * alphas(:)
+          end do
+        end if
+      end if
+      K = K**zeta
+    end if
+
+!    V = delta**2 * matmul( K, alphas ) + V0
+    if( n_sites > 0 )then
+      call dgemm( "n", "n", n_sites, 1, n_sparse, delta**2, K, n_sites, alphas, n_sparse, 0.d0, V, n_sites)
+    end if
+    V = V + V0
+
+!   Make sure all V are >= 0
+    do i = 1, size(V)
+      if( V(i) < 0.d0 )then
+        V(i) = 0.d0
+      end if
+    end do
+
+    if( do_derivatives)then
+      if( n_sites > 0 )then
+        call dgemm("n", "t", n_sites, n_soap, n_sparse, delta**2, K_der, n_sites, &
+                   Qs_copy, n_soap, 0.d0, Qss, n_sites)
+      end if
+      j = 1
+      do i = 1, n_sites
+        do i2 = 1, n_neigh(i)
+          if( V(i) == 0.d0 )then
+            V_der(1:3, j) = 0.d0
+          else
+             do cart = 1, 3
+                V_der(cart, j) = dot_product( Qss(i,:), soap_cart_der(cart, :, j) )
+            end do
+          end if
+          j = j + 1
+        end do
+      end do
+      deallocate(Qs_copy)
+      deallocate(Qss)
+      deallocate(K_der)
+    end if
+  deallocate(K)
+
+  end subroutine
+!**************************************************************************
+
+end module
