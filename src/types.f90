dec85489828831e3a8a508cc9f66536fb4dce80f
Muhli Heikki <muhlih1@l23-0711.org.aalto.fi>
2026-03-13 16:07:48 +0200

    Turbogap school

--- src/types.f90
+++ src/types.f90
@@ -85,7 +85,7 @@ module types
               box_scaling_factor(3,3) = reshape([1.d0, 0.d0, 0.d0, 0.d0, 1.d0, 0.d0, 0.d0, 0.d0, 1.d0], [3,3]), &
               core_pot_cutoff = 1.d10, core_pot_buffer = 1.d0, tau_dt = 100.d0, target_pos_step, &
               gamma0 = 0.01d0, max_opt_step = 0.1d0, vdw_scs_rcut = 4.5d0, vdw_mbd_rcut = 5.d0, vdw_mbd_rcut2 = 5.d0, &
-              vdw_2b_rcut = 10.d0, vdw_2b_rcut2 = 10.d0, vdw_omega_ref = 4.d0, vdw_loc_rcut = 4.5d0, &
+              vdw_2b_rcut = 10.d0, vdw_2b_rcut2 = 10.d0, vdw_omega_ref = 1.3d0, vdw_loc_rcut = 4.5d0, &
               vdw_d_mbd = 6.d0, vdw_sr_mbd = 0.83d0, poly_cut_xmin = 3.d0, poly_cut_xmax = 10.d0
     integer :: md_nsteps = 1, write_xyz = 0, write_thermo = 1, which_atom = 0, vdw_mbd_nfreq = 12, vdw_mbd_norder = 4, &
                mbd_correction_freq = 10
@@ -101,8 +101,8 @@ module types
                write_virial = .true., write_pressure = .true., write_stress = .true., &
                write_local_energies = .true., write_property(1:11) = .true., &
                write_array_property(1:8) = .true., write_masses = .false., write_fixes = .true., &
-               variable_time_step = .false., vdw_mbd_grad = .false., vdw_hirsh_grad = .false., &
-               vdw_polynomial = .false., do_nnls = .true., vdw_mbd_cent_appr = .true.
+               variable_time_step = .false., vdw_mbd_grad = .true., vdw_hirsh_grad = .true., &
+               vdw_polynomial = .false., do_nnls = .false., vdw_mbd_cent_appr = .true.
   end type input_parameters
 
 end module
